b110018ef3b1495c4ca76e31890cb590aa92a98d
rweventos <rweventos.rj@gmail.com>
2023-10-22 11:54:28 -0300

    feat: added product page

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -2,6 +2,9 @@ import { db } from "@/lib/prisma";
 import ProductShowcase from "./component/product-showcase";
 import TitleHeader from "@/components/ui/title-header";
 import ProductList from "@/app/(home)/components/product-list";
+import ProductInfo from "./component/product-info";
+import { computeProductTotalPrice } from "@/helper/product";
+import NoProductFound from "./component/not-founded";
 
 type ProductPageProps = {
   params: {
@@ -10,12 +13,28 @@ type ProductPageProps = {
 };
 
 const ProductPage = async ({ params }: ProductPageProps) => {
+  //this is a better way to select one product and all related products of the same category excluding the current product
+  //this way we don't need to make two queries
   const product = await db.product.findFirst({
     where: {
       slug: params.slug,
     },
+    include: {
+      category: {
+        include: {
+          products: {
+            where: {
+              NOT: {
+                slug: params.slug,
+              },
+            },
+          },
+        },
+      },
+    },
   });
-  const relatedProducts = await db.product.findMany({
+
+  /*   const relatedProducts = await db.product.findMany({
     where: {
       categoryId: product?.categoryId,
       NOT: {
@@ -23,16 +42,16 @@ const ProductPage = async ({ params }: ProductPageProps) => {
       },
     },
     take: 4,
-  });
-
+  }); */
+  if (!product) return <NoProductFound />;
   return (
     <>
       <div className="flex flex-col">
-        {product && <ProductShowcase product={product} />}
+        <ProductShowcase product={product} />
       </div>
-
+      <ProductInfo product={computeProductTotalPrice(product)} />
       <TitleHeader title="FEATURED PRODUCTS" />
-      <ProductList products={relatedProducts} />
+      <ProductList products={product.category.products} />
     </>
   );
 };
