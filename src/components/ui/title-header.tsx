465321adb0c8bb6c00c73a91b0d8d430b9a14e06
rweventos <rweventos.rj@gmail.com>
2023-10-22 06:38:59 -0300

    fix: added className functionality to component

--- src/components/ui/title-header.tsx
+++ src/components/ui/title-header.tsx
@@ -1,11 +1,16 @@
+import { cn } from "@/lib/utils";
+
 type TitleHeaderProps = {
   title: string;
   subtitle?: string;
+  className?: string;
 };
 
-const TitleHeader = ({ title, subtitle }: TitleHeaderProps) => {
+const TitleHeader = ({ title, subtitle, className }: TitleHeaderProps) => {
   return (
-    <div className="mb-5 mt-7 flex flex-col justify-start px-5">
+    <div
+      className={cn(`mb-5 mt-7 flex flex-col justify-start px-5`, className)}
+    >
       <h1 className="text-lg font-semibold">{title}</h1>
       {subtitle && <h2>{subtitle}</h2>}
     </div>
