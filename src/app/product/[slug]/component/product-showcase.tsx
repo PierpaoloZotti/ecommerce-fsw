8df9fc81c2e4ab55e1aab77de1a87652526611b4
rweventos <rweventos.rj@gmail.com>
2023-10-22 06:39:58 -0300

    feat: added functionality to component to choose the main img

--- src/app/product/[slug]/component/product-showcase.tsx
+++ src/app/product/[slug]/component/product-showcase.tsx
@@ -2,17 +2,19 @@
 
 import { Product } from "@prisma/client";
 import Image from "next/image";
+import { useState } from "react";
 
 type ProductShowcaseProps = {
   product: Product;
 };
 
 const ProductShowcase = ({ product }: ProductShowcaseProps) => {
+  const [mainImage, setMainImage] = useState(product.imageUrls[0]);
   return (
     <div>
       <div className="flex h-[380px] w-full flex-col items-center justify-center bg-accent">
         <Image
-          src={product.imageUrls[0]}
+          src={mainImage}
           alt={product.slug}
           height={0}
           width={0}
@@ -21,11 +23,12 @@ const ProductShowcase = ({ product }: ProductShowcaseProps) => {
         />
       </div>
       <div className="-mt-8 rounded-t-xl bg-background">
-        <div className="flex items-center justify-between rounded-xl bg-[#0b0b0b] px-5 py-8">
+        <div className="flex items-center justify-between rounded-xl bg-background px-5 py-8">
           {product.imageUrls.map((image) => (
             <div
               key={image}
-              className="relative flex h-[77px] w-[77px] items-center justify-center rounded-xl bg-accent"
+              className="relative flex h-[77px] w-[77px] cursor-pointer items-center justify-center rounded-xl bg-accent"
+              onClick={() => setMainImage(image)}
             >
               <Image
                 src={image}
